1a983b875e12e3300ace6c14f1c851570bc9f1ad
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-06-03 23:19:52 -0300

    drop the column "subject" before create "subject" table

--- src/migrations/1653258996093-addOptionsInSomeEntities.ts
+++ src/migrations/1653258996093-addOptionsInSomeEntities.ts
@@ -1,24 +1,38 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class addOptionsInSomeEntities1653258996093 implements MigrationInterface {
-    name = 'addOptionsInSomeEntities1653258996093'
+export class addOptionsInSomeEntities1653258996093
+  implements MigrationInterface
+{
+  name = 'addOptionsInSomeEntities1653258996093';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "subject" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "isPremium" boolean NOT NULL, CONSTRAINT "PK_12eee115462e38d62e5455fc054" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "subject"`);
-        await queryRunner.query(`ALTER TABLE "trivia" ADD "type" character varying NOT NULL DEFAULT 'TRIVIA'`);
-        await queryRunner.query(`ALTER TABLE "trivia" ADD "source" character varying`);
-        await queryRunner.query(`ALTER TABLE "trivia" ADD "subject_id" integer`);
-        await queryRunner.query(`ALTER TABLE "trivia" ADD CONSTRAINT "FK_99758a4bbda0c8c35780bde5357" FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "subject"`);
+    await queryRunner.query(
+      `CREATE TABLE "subject" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "isPremium" boolean NOT NULL, CONSTRAINT "PK_12eee115462e38d62e5455fc054" PRIMARY KEY ("id"))`,
+    );
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "trivia" DROP CONSTRAINT "FK_99758a4bbda0c8c35780bde5357"`);
-        await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "subject_id"`);
-        await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "source"`);
-        await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "type"`);
-        await queryRunner.query(`ALTER TABLE "trivia" ADD "subject" character varying NOT NULL`);
-        await queryRunner.query(`DROP TABLE "subject"`);
-    }
+    await queryRunner.query(
+      `ALTER TABLE "trivia" ADD "type" character varying NOT NULL DEFAULT 'TRIVIA'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "trivia" ADD "source" character varying`,
+    );
+    await queryRunner.query(`ALTER TABLE "trivia" ADD "subject_id" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "trivia" ADD CONSTRAINT "FK_99758a4bbda0c8c35780bde5357" FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "trivia" DROP CONSTRAINT "FK_99758a4bbda0c8c35780bde5357"`,
+    );
+    await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "subject_id"`);
+    await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "source"`);
+    await queryRunner.query(`ALTER TABLE "trivia" DROP COLUMN "type"`);
+    await queryRunner.query(
+      `ALTER TABLE "trivia" ADD "subject" character varying NOT NULL`,
+    );
+    await queryRunner.query(`DROP TABLE "subject"`);
+  }
 }
