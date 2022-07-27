4f03dc519e3654d454640ff9910b8e431da815a7
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-27 20:34:50 -0300

    Delete - The TriviasModule wasn't use in the courses module, so the import was  innecesary

--- src/courses/courses.resolver.ts
+++ src/courses/courses.resolver.ts
@@ -3,14 +3,19 @@ import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
 import { CreateCourseInput } from './dto/create-course.input';
 import { UpdateCourseInput } from './dto/update-course.input';
-import { ValidationPipe } from '@nestjs/common';
+import { UseGuards, ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver(() => Course)
 export class CoursesResolver {
-  constructor(private readonly coursesService: CoursesService) { }
+  constructor(private readonly coursesService: CoursesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Course, { name: 'createCourse' })
-  createCourse(@Args('createCourseInput', new ValidationPipe()) createCourseInput: CreateCourseInput) {
+  createCourse(
+    @Args('createCourseInput', new ValidationPipe())
+    createCourseInput: CreateCourseInput,
+  ) {
     return this.coursesService.create(createCourseInput);
   }
 
@@ -25,7 +30,10 @@ export class CoursesResolver {
   }
 
   @Mutation(() => Course, { name: 'updateCourse' })
-  updateCourse(@Args('updateCourseInput', new ValidationPipe()) updateCourseInput: UpdateCourseInput) {
+  updateCourse(
+    @Args('updateCourseInput', new ValidationPipe())
+    updateCourseInput: UpdateCourseInput,
+  ) {
     return this.coursesService.update(updateCourseInput.id, updateCourseInput);
   }
 
