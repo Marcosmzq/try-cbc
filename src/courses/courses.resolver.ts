6ac6ef2e8d72d37edfcf4db9368e5d6acb6419db
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-30 18:44:50 -0300

    Add - Guards implemented for every method

--- src/courses/courses.resolver.ts
+++ src/courses/courses.resolver.ts
@@ -5,12 +5,14 @@ import { CreateCourseInput } from './dto/create-course.input';
 import { UpdateCourseInput } from './dto/update-course.input';
 import { UseGuards, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AdminAuthGuard } from 'src/auth/guards/admin-role.guard';
+import { CourseByIdPipe } from './pipes/course-by-id.pipe';
 
 @Resolver(() => Course)
 export class CoursesResolver {
   constructor(private readonly coursesService: CoursesService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => Course, { name: 'createCourse' })
   createCourse(
     @Args('createCourseInput', new ValidationPipe())
@@ -19,24 +21,29 @@ export class CoursesResolver {
     return this.coursesService.create(createCourseInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Course], { name: 'findAllCourses' })
   findAll() {
     return this.coursesService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => Course, { name: 'findCourseByID' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.coursesService.findOne(id);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => Course, { name: 'updateCourse' })
   updateCourse(
     @Args('updateCourseInput', new ValidationPipe())
     updateCourseInput: UpdateCourseInput,
+    @Args('course_id', CourseByIdPipe) course: Course,
   ) {
-    return this.coursesService.update(updateCourseInput.id, updateCourseInput);
+    return this.coursesService.update(course, updateCourseInput);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => Course, { name: 'deleteCourse' })
   removeCourse(@Args('id', { type: () => Int }) id: number) {
     return this.coursesService.remove(id);
