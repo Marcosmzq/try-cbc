1ad3211c99e7634ef942bd6b8f0e1cf511de7551
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-30 18:48:34 -0300

    Delete - Remove unused files

--- src/docorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../users/enums/userRole.enum';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
