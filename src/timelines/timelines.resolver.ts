8b849af866d0a5eb7b8c8bdc1e61f2f4584d2242
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-30 18:47:50 -0300

    Add - Guards implemented for every method

--- src/timelines/timelines.resolver.ts
+++ src/timelines/timelines.resolver.ts
@@ -3,40 +3,73 @@ import { TimelinesService } from './timelines.service';
 import { Timeline } from './entities/timeline.entity';
 import { CreateTimelineInput } from './dto/create-timeline.input';
 import { UpdateTimelineInput } from './dto/update-timeline.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Course } from 'src/courses/entities/course.entity';
+import { CourseByIdPipe } from 'src/courses/pipes/course-by-id.pipe';
+import { AdminAuthGuard } from 'src/auth/guards/admin-role.guard';
+import { TimelinesCrudOpsGuard } from './guards/timeline-crud-ops.guard';
+import { TimelineByIdPipe } from './pipes/timeline-by-id.pipe';
 
 @Resolver(() => Timeline)
 export class TimelinesResolver {
   constructor(private readonly timelinesService: TimelinesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Timeline, { name: 'createTimeline' })
   createTimeline(
+    @CurrentUser() currentUser: User,
+    @Args('course_id', { type: () => Int }, CourseByIdPipe) course: Course,
     @Args('createTimelineInput') createTimelineInput: CreateTimelineInput,
   ) {
-    return this.timelinesService.create(createTimelineInput);
+    return this.timelinesService.create(
+      currentUser,
+      course,
+      createTimelineInput,
+    );
   }
 
+  @UseGuards(AdminAuthGuard)
   @Query(() => [Timeline], { name: 'findAllTimelines' })
   findAll() {
     return this.timelinesService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard, TimelinesCrudOpsGuard)
   @Query(() => Timeline, { name: 'findTimelineById' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.timelinesService.findOne(id);
+  findOne(@Args('timeline_id', { type: () => Int }) timeline_id: number) {
+    return this.timelinesService.findOne(timeline_id);
   }
 
-  @Mutation(() => Timeline, { name: 'updateTimeline' })
-  updateTimeline(
-    @Args('updateTimelineInput') updateTimelineInput: UpdateTimelineInput,
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [Timeline], { name: 'findCurrentUserTimelinesByCourse' })
+  findCurrentUserTimelinesByCourse(
+    @CurrentUser() currentUser: User,
+    @Args('course_id', { type: () => Int }, CourseByIdPipe) course: Course,
   ) {
-    return this.timelinesService.update(
-      updateTimelineInput.id,
-      updateTimelineInput,
+    return this.timelinesService.findCurrentUserTimelinesByCourse(
+      currentUser,
+      course,
     );
   }
 
+  @UseGuards(JwtAuthGuard, TimelinesCrudOpsGuard)
+  @Mutation(() => Timeline, { name: 'updateTimeline' })
+  updateTimeline(
+    @Args('timeline_id', { type: () => Int }, TimelineByIdPipe)
+    timeline: Timeline,
+    @Args('updateTimelineInput') updateTimelineInput: UpdateTimelineInput,
+  ) {
+    return this.timelinesService.update(timeline, updateTimelineInput);
+  }
+
+  @UseGuards(JwtAuthGuard, TimelinesCrudOpsGuard)
   @Mutation(() => Timeline, { name: 'deleteTimeline' })
-  removeTimeline(@Args('id', { type: () => Int }) id: number) {
-    return this.timelinesService.remove(id);
+  removeTimeline(
+    @Args('timeline_id', { type: () => Int }) timeline_id: number,
+  ) {
+    return this.timelinesService.remove(timeline_id);
   }
 }
