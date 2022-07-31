90c69b22ae36e687c75135371e6fee5bc3fd335f
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-31 00:39:02 -0300

    Update - Removed @Roles decorator from this resolver. Add - Guards in every methos, and in some other pipes.

--- src/trivias-answers/trivias-answers.resolver.ts
+++ src/trivias-answers/trivias-answers.resolver.ts
@@ -3,49 +3,54 @@ import { TriviasAnswersService } from './trivias-answers.service';
 import { TriviasAnswer } from './entities/trivias-answer.entity';
 import { CreateTriviasAnswerInput } from './dto/create-trivias-answer.input';
 import { UpdateTriviasAnswerInput } from './dto/update-trivias-answer.input';
-import { Roles } from 'src/docorators/roles.decorator';
-import { UserRole } from 'src/users/enums/userRole.enum';
-import { ValidationPipe } from '@nestjs/common';
+import { UseGuards, ValidationPipe } from '@nestjs/common';
+import { AdminAuthGuard } from 'src/auth/guards/admin-role.guard';
+import { TriviaByIdPipe } from 'src/trivias/pipes/trivia-by-id.pipe';
+import { Trivia } from 'src/trivias/entities/trivia.entity';
+import { TriviaAnswerByIdPipe } from './pipes/trivia-answer-by-id.pipe';
 
 @Resolver(() => TriviasAnswer)
 export class TriviasAnswersResolver {
   constructor(private readonly triviasAnswersService: TriviasAnswersService) {}
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => TriviasAnswer, { name: 'createTriviaAnswer' })
-  @Roles(UserRole.ADMIN)
   createTriviasAnswer(
+    @Args('trivia_id', { type: () => Int }, TriviaByIdPipe) trivia: Trivia,
     @Args('createTriviasAnswerInput', new ValidationPipe())
     createTriviasAnswerInput: CreateTriviasAnswerInput,
   ) {
-    return this.triviasAnswersService.create(createTriviasAnswerInput);
+    return this.triviasAnswersService.create(trivia, createTriviasAnswerInput);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Query(() => [TriviasAnswer], { name: 'findAllTriviasAnswers' })
-  @Roles(UserRole.ADMIN)
   findAll() {
     return this.triviasAnswersService.findAll();
   }
 
+  @UseGuards(AdminAuthGuard)
   @Query(() => TriviasAnswer, { name: 'findTriviasAnswerByID' })
-  @Roles(UserRole.ADMIN)
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.triviasAnswersService.findOne(id);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => TriviasAnswer, { name: 'updateTriviaAnswer' })
-  @Roles(UserRole.ADMIN)
   updateTriviasAnswer(
+    @Args('trivaAnswer_id', { type: () => Int }, TriviaAnswerByIdPipe)
+    triviaAnswer: TriviasAnswer,
     @Args('updateTriviasAnswerInput', new ValidationPipe())
     updateTriviasAnswerInput: UpdateTriviasAnswerInput,
   ) {
     return this.triviasAnswersService.update(
-      updateTriviasAnswerInput.id,
+      triviaAnswer,
       updateTriviasAnswerInput,
     );
   }
 
+  @UseGuards(AdminAuthGuard)
   @Mutation(() => TriviasAnswer, { name: 'deleteTriviaAnswer' })
-  @Roles(UserRole.ADMIN)
   removeTriviasAnswer(@Args('id', { type: () => Int }) id: number) {
     return this.triviasAnswersService.remove(id);
   }
