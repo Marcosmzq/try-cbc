6c915aa5a4eaa24af52cc3ba6a55c3453204d040
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-31 00:40:11 -0300

    Update - The triviaRepository is not longer more a dependency in this service. Also the code was improved a little.

--- src/trivias-answers/trivias-answers.service.ts
+++ src/trivias-answers/trivias-answers.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserInputError } from 'apollo-server-express';
 import { Trivia } from 'src/trivias/entities/trivia.entity';
 import { Repository } from 'typeorm';
 import { CreateTriviasAnswerInput } from './dto/create-trivias-answer.input';
@@ -10,19 +9,15 @@ import { TriviasAnswer } from './entities/trivias-answer.entity';
 @Injectable()
 export class TriviasAnswersService {
   constructor(
-    @InjectRepository(Trivia)
-    private readonly triviaRepository: Repository<Trivia>,
     @InjectRepository(TriviasAnswer)
     private readonly triviasAnswerRepository: Repository<TriviasAnswer>,
   ) {}
 
-  async create(createTriviasAnswerInput: CreateTriviasAnswerInput) {
-    const { statement, isTrue, trivia_id } = createTriviasAnswerInput;
-    const trivia = await this.triviaRepository.findOne(trivia_id);
-    if (!trivia)
-      throw new UserInputError(
-        'The trivia id entered not belongs to any trivia.',
-      );
+  async create(
+    trivia: Trivia,
+    createTriviasAnswerInput: CreateTriviasAnswerInput,
+  ) {
+    const { statement, isTrue } = createTriviasAnswerInput;
     const answer = this.triviasAnswerRepository.create({
       statement,
       isTrue,
@@ -40,13 +35,11 @@ export class TriviasAnswersService {
     return this.triviasAnswerRepository.findOne(id);
   }
 
-  async update(id: number, updateTriviasAnswerInput: UpdateTriviasAnswerInput) {
+  async update(
+    answer: TriviasAnswer,
+    updateTriviasAnswerInput: UpdateTriviasAnswerInput,
+  ) {
     const { statement, isTrue } = updateTriviasAnswerInput;
-    const answer = await this.triviasAnswerRepository.findOne(id);
-    if (!answer)
-      throw new UserInputError(
-        'The trivia answer id entered not belongs to any answer.',
-      );
     if (statement) answer.statement = statement;
     if (isTrue !== undefined) answer.isTrue = isTrue;
     return this.triviasAnswerRepository.save(answer);
