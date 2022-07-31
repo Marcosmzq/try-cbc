8e84c55244fdf5491a1655f8c44234f592b1e16f
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-07-31 00:32:41 -0300

    Update - The unused imports was removed from the Trivia and TriviaAnswers modules.

--- src/trivias-answers/trivias-answers.module.ts
+++ src/trivias-answers/trivias-answers.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TriviasAnswersService } from './trivias-answers.service';
 import { TriviasAnswersResolver } from './trivias-answers.resolver';
-import { Trivia } from 'src/trivias/entities/trivia.entity';
 import { TriviasAnswer } from './entities/trivias-answer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trivia, TriviasAnswer])],
+  imports: [TypeOrmModule.forFeature([TriviasAnswer])],
   providers: [TriviasAnswersResolver, TriviasAnswersService],
 })
 export class TriviasAnswersModule {}
--- src/trivias/trivias.module.ts
+++ src/trivias/trivias.module.ts
@@ -3,11 +3,9 @@ import { TriviasService } from './trivias.service';
 import { TriviasResolver } from './trivias.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Trivia } from './entities/trivia.entity';
-import { TriviasAnswersModule } from 'src/trivias-answers/trivias-answers.module';
-import { Course } from 'src/courses/entities/course.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trivia, Course]), TriviasAnswersModule],
+  imports: [TypeOrmModule.forFeature([Trivia])],
   providers: [TriviasResolver, TriviasService],
 })
 export class TriviasModule {}
