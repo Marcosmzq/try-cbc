ee66e0ac7402c95cc04c048e3ef65ea459385899
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-08-02 17:21:44 -0300

    Update - Now in prod env, the ormconfig only need a url for conect to a db

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -13,11 +13,7 @@ ConfigModule.forRoot();
 
 const ormConfigProd: ConnectionOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DB_URL_CONNECTION,
   synchronize: false,
   ssl: true,
   migrationsRun: true,
