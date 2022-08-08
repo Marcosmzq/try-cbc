684b04090f246c1af40209ffc75a15a04215047e
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-08-08 18:04:02 -0300

    Update - Now the preference back urls work with the new client

--- src/checkout/config/preference-default.config.ts
+++ src/checkout/config/preference-default.config.ts
@@ -5,19 +5,19 @@ export const preferenceDefaultConfig = (userID: number) => {
     external_reference: userID.toString(),
     items: [
       {
-        title: 'Potenciar de tryCBC y obtener beneficios únicos',
+        title: 'Comprar el curso tryCBC y aprobá fácil.',
         category_id: 'learnings',
         currency_id: 'ARS',
-        unit_price: 2000,
+        unit_price: 2499,
         quantity: 1,
         description:
-          'Deja de sufrir estudiando y APROBA MÁS FÁCIL con las nuevas funciones que tenemos preparadas para vos',
+          'Deja de sufrir estudiando y APROBA MÁS FÁCIL con las funciones que tenemos preparadas para vos',
       },
     ],
     back_urls: {
-      success: `${process.env.CLIENT_URL}/premium/buy-response`,
-      failure: `${process.env.CLIENT_URL}/premium/buy-response`,
-      pending: `${process.env.CLIENT_URL}/premium/buy-response`,
+      success: `${process.env.CLIENT_URL}/buy-response`,
+      failure: `${process.env.CLIENT_URL}/buy-response`,
+      pending: `${process.env.CLIENT_URL}/buy-response`,
     },
     auto_return: 'approved',
   };
